a48ca7f3556142735bd3c582f8674d0434bf0824
Rodrigo Saldias <rodrigo.saldias@hotmail.cl>
2016-09-24 04:38:35 -0400

    Modificaciones varias

--- src/app/components/services/EmpresasService.js
+++ src/app/components/services/EmpresasService.js
@@ -17,6 +17,22 @@
 			insertEmpresas : function(empresa) {
 				empresa.id = empresas.length + 1;
 				empresas.push(empresa);
+			},
+			getOneById : function(id) {
+				console.log(parseInt(id), empresas);
+				return $q.when(_.findWhere(empresas, {'id': parseInt(id)}));
+			},
+			saveEmpresa : function(empresa) {
+				var deferred = $q.defer();
+				var oldEmpresa = _.findWhere(empresas, {'id': empresa.id});
+				if (!oldEmpresa) {
+					deferred.reject({});
+				} else {
+					var index = _.indexOf(empresas, oldEmpresa);
+					empresas[index] = empresa;
+					deferred.resolve(empresa);
+				}
+				return deferred.promise;
 			}
 		};
 	}	
--- src/app/controllers/EmpresaController.js
+++ src/app/controllers/EmpresaController.js
@@ -7,7 +7,7 @@
 		.controller('NuevaEmpresaController', [ '$state', 'empresasService',
 			NuevaEmpresaController
 		])
-		.controller('EditarEmpresaController', [
+		.controller('EditarEmpresaController', [ '$stateParams', 'empresasService', '$state',
           EditarEmpresaController
 		]);
 
@@ -23,22 +23,51 @@
 	function NuevaEmpresaController($state, empresasService) {
 		var vm = this;
 		vm.createEmpresa = createEmpresa;
-		
+		vm.formEmpresa = {};
+		vm.formatoInputs = {
+			name: '^[A-Za-z\\d\\s]+$'
+		};
 		vm.empresa = { 
 			name: '', 
-			codeSoftalnd: ''
+			code: ''
 		};
 		
 		function createEmpresa() {
-			console.log(vm.empresa);
 			empresasService.insertEmpresas(vm.empresa);
 			$state.go('home.empresas');
 		}
 	}
 	
-	function EditarEmpresaController() {
+	function EditarEmpresaController($stateParams, empresasService, $state) {
 		var vm = this;
-
+		vm.guardarEmpresa = guardarEmpresa;
+		
+		vm.empresa = { 
+			name: '', 
+			code: ''
+		};
+		console.log($stateParams.id)
+		if ($stateParams.id) {
+			empresasService.getOneById($stateParams.id)
+			.then(function (empresa) {
+				console.log(empresa);
+				vm.empresa = empresa;
+			}, function(error) {
+				console.log('No exito al guardar', error);
+			});		
+		} else {
+			$state.go('home.empresas');
+		}
+		
+		function guardarEmpresa() {
+			console.log('Inicia guardado');
+			empresasService.saveEmpresa(vm.empresa)
+			.then(function (empresa) {
+				console.log('Exito al guardar', empresa);
+				$state.go('home.empresas');
+			}, function(error) {
+				console.log('No exito al guardar', error);
+			});
+		}
 	}
-
 })();
