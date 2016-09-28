cb27fc62cd09dad0d198641e32b4c8f42607625c
Rodrigo Saldias <rodrigo.saldias@hotmail.cl>
2016-09-28 10:11:44 -0400

    se actualizo la forma de consumir servicios, basandose en el modulo de empresas

--- src/app/components/services/PlanCtaChileService.js
+++ src/app/components/services/PlanCtaChileService.js
@@ -3,59 +3,88 @@
 
 	angular.module('app')
         .service('planCtaChileService', [
-        '$q', '$http',
+        '$q', '$http', 'apiRoutes',
         planCtaChileService
 	]);
 
-	function planCtaChileService($q, $http) {
-		return {
-			loadAllPlanCtaChile : function() {
-				return $q.when(
-					$http.get('https://megacentroapi-mosschile.rhcloud.com/api/chileanAccount')
-				);
-			},
-			insertPlanCtaChile : function(planCtaChile) {
-				var now = new Date();
-				var data = {
-					account: planCtaChile.account,
-					name: planCtaChile.name,
-					level1: planCtaChile.level1,
-					level2: planCtaChile.level2,
-					level3: planCtaChile.level3,
-					status: "active",
-					createDate: now.toString()
-				};
-				return $q.when(
-					$http.post(
-						'https://megacentroapi-mosschile.rhcloud.com/api/chileanAccount',
-						data
-					)
-				);
-			},
-			updateEmpresa: function(planCtaChile) {
-				var data = {
-          account: planCtaChile.account,
-				  name: planCtaChile.name,
-          level1: planCtaChile.level1,
-          level2: planCtaChile.level2,
-          level3: planCtaChile.level3
-				};
-				return $q.when(
-					$http.put(
-						'https://megacentroapi-mosschile.rhcloud.com/api/chileanAccount/' + planCtaChile.id.toString(),
-						data
-					)
-				);
-			},
-			getById : function(id) {
-				return $q.when(
-					$http.get(
-						'https://megacentroapi-mosschile.rhcloud.com/api/chileanAccount/' + id
-					)
-				);
-			},
-			deletePlanCtaChile : function(planCtaChile) {
-			}
-		};
+	function planCtaChileService($q, $http, apiRoutes) {
+			return {
+					loadAllPlanCtaChile : function() {
+							return $q.when(
+									$http.get(
+											apiRoutes.chileanAccount,
+											{
+												params: {
+														'filter[where][status]': 'active'
+												}
+											}
+									)
+							);
+					},
+					insertPlanCtaChile : function(planCtaChile) {
+							var now = new Date();
+							var data = {
+									account: planCtaChile.account,
+									name: planCtaChile.name,
+									level1: planCtaChile.level1,
+									level2: planCtaChile.level2,
+									level3: planCtaChile.level3,
+									status: "active",
+									createDate: now.toString()
+							};
+							return $q.when(
+									$http.post(
+											apiRoutes.chileanAccount,
+											data
+									)
+							);
+					},
+					updateEmpresa: function(planCtaChile) {
+							var data = {
+		          		account: planCtaChile.account,
+						  		name: planCtaChile.name,
+		          		level1: planCtaChile.level1,
+		          		level2: planCtaChile.level2,
+		          		level3: planCtaChile.level3
+							};
+							return $q.when(
+									$http.put(
+											apiRoutes.chileanAccount + '/' + planCtaChile.id.toString(),
+											data
+									)
+							);
+					},
+					getById : function(id) {
+							return $q.when(
+									$http.get(
+											apiRoutes.chileanAccount + '/' + id
+									)
+							);
+					},
+					deletePlanCtaChile : function(planCtaChile) {
+							var data = {
+									status: 'eliminada'
+							};
+							return $q.when(
+									$http.put(
+											apiRoutes.chileanAccount + '/' + planCtaChile.id.toString(),
+											data
+									)
+							);
+					},
+					findLikeName: function(text) {
+							return $q.when(
+									$http.get(
+											apiRoutes.chileanAccount,
+											{
+													params: {
+															'filter[where][and][0][name][like]': text + '%',
+															'filter[where][and][1][status]': 'active'
+													}
+											}
+									)
+							);
+					}
+			};
 	}
 })();
--- src/app/controllers/PlanChileController.js
+++ src/app/controllers/PlanChileController.js
@@ -2,15 +2,19 @@
   angular
        .module('app')
        .controller('PlanChileController', [ 'planCtaChileService', '$mdDialog',
-          PlanChileController
+            PlanChileController
        ])
        .controller('NuevoPlanChileController', [ '$state', 'planCtaChileService', 'formatosImputs',
-				  '$rootScope',
-          NuevoPlanChileController
+				    '$rootScope',
+            NuevoPlanChileController
        ])
-       .controller('EditarPlanChileController', [
-          EditarPlanChileController
-       ]);
+       .controller('EditarPlanChileController', [ '$stateParams', '$state', 'planCtaChileService', 'formatosImputs',
+				    '$rootScope',
+            EditarPlanChileController
+       ])
+       .controller('EliminarPlanChileController', [ '$stateParams',
+				    EliminarPlanChileController
+		   ]);
 
   function PlanChileController(planCtaChileService, $mdDialog) {
 		  var vm = this;
@@ -19,13 +23,13 @@
 		  vm.tableData = [];
 		  vm.isOpen = false;
 		  function cargaInicial() {
-  			planCtaChileService.loadAllPlanCtaChile()
-  			.then(function (planCtaChile) {
-    				vm.tableData = planCtaChile.data.map(function(planCtaChile) {
-      					planCtaChile.open = false;
-      					return planCtaChile;
-    				});
-  			});
+  			   planCtaChileService.loadAllPlanCtaChile()
+  			   .then(function (planCtaChile) {
+    				    vm.tableData = planCtaChile.data.map(function(planCtaChile) {
+      					    planCtaChile.open = false;
+      					    return planCtaChile;
+    				    });
+  			   });
 		  }
 		  cargaInicial();
 		  function openMenu($mdOpenMenu, event) {
@@ -33,45 +37,91 @@
           $mdOpenMenu(event);
   		}
 		  function deletePlanCtaChile(planCtaChile, event) {
-  			var confirm = $mdDialog.confirm()
+  			   var confirm = $mdDialog.confirm()
   	          .title('PlanesCtaChile')
   	          .textContent('¿Desea eliminar el Plan Cta de Chile ' + planCtaChile.name + ' definitivamente?')
   	          .ariaLabel('Lucky day')
   	          .targetEvent(event)
   	          .ok('Confirmar')
   	          .cancel('Cancelar');
-  	    $mdDialog.show(confirm).then(function() {
+  	       $mdDialog.show(confirm).then(function() {
   				    planCtaChileService.deletePlanCtaChile(planCtaChile);
-  	    });
+              .then(function() {
+					          cargaInicial();
+					          $mdDialog.show(
+		      	             $mdDialog.alert()
+			                   .clickOutsideToClose(true)
+			                   .title('Eliminando Plan Cta de Chile')
+			                   .textContent('Plan Cta de Chile ' + planCtaChile.name + ' eliminada con éxito.')
+			                   .ok('Ok')
+		    	          );
+				      });
+  	       });
   		}
 	}
 
   function NuevoPlanChileController($state, planCtaChileService, formatosImputs, rootScope) {
-		var vm = this;
-		vm.createPlanCtaChile = createPlanCtaChile;
+		  var vm = this;
+		  vm.createPlanCtaChile = createPlanCtaChile;
 
-		vm.formPlanChile = {};
-		vm.formatosImputs = formatosImputs.formPlanChile;
-		vm.planCtaChile = {
-      account: '',
-			name: '',
-			level1: '',
-			level2: '',
-			level3: '',
-		};
+  		vm.formPlanChile = {};
+  		vm.formatosImputs = formatosImputs.formPlanChile;
+  		vm.planCtaChile = {
+          account: '',
+    			name: '',
+    			level1: '',
+    			level2: '',
+    			level3: '',
+  		};
 
-		function createPlanCtaChile() {
-			planCtaChileService.insertPlanCtaChile(vm.planCtaChile)
-			.then(function() {
-				console.log('Exito');
-				rootScope.$broadcast(
-					'event:toastMessage',
-					'Plan Cta. de Chile registrada con éxito.',
-					'md-primary'
-				);
-				$state.go('home.plan-chile', {}, {location: 'replace'});
-			});
-		}
+  		function createPlanCtaChile() {
+    			planCtaChileService.insertPlanCtaChile(vm.planCtaChile)
+    			.then(function() {
+    				  console.log('Exito');
+    				  rootScope.$broadcast(
+    					    'event:toastMessage',
+        					'Plan Cta. de Chile registrada con éxito.',
+        					'md-primary'
+    				  );
+    				  $state.go('home.plan-chile', {}, {location: 'replace'});
+    			});
+  		}
 	}
+
+  function EditarPlanChileController(stateParams, state, planCtaChileService, formatosImputs, rootScope) {
+      var vm = this;
+      //Agregando codigo para probar las validaciones.
+      vm.formatosImputs = formatosImputs.formEditar;
+      vm.planCtaChile = {
+          account: '',
+    			name: '',
+    			level1: '',
+    			level2: '',
+    			level3: '',
+  		};
+      vm.handleSubmit = guardarEmpresa;
+      vm.formEditar = {};
+
+      if (stateParams.id) {
+          planCtaChileService.getById(stateParams.id)
+          .then(function(getResult) {
+              vm.planCtaChile = getResult.data;
+          });
+      } else {
+        state.go('home.plan-chile');
+      }
+
+      function guardarPlanCtaChile() {
+          planCtaChileService.updatePlanCtaChile(vm.planCtaChile)
+          .then(function(updateResult) {
+              rootScope.$broadcast(
+                'event:toastMessage',
+                'Datos guardados con éxito.',
+                'md-primary'
+              );
+              state.go('home.plan-chile');
+          })
+      }
+  }
 
 })();
