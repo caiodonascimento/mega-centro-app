ca9a5557cb4ee23d44b772790ac17d3a2caa8786
Caio Medeiros <caio.dona@gmail.com>
2016-11-10 00:04:20 -0300

    Modificaciones a Gestión de Movimientos

--- src/app/components/services/MovimientoService.js
+++ src/app/components/services/MovimientoService.js
@@ -21,20 +21,49 @@
             );
           },
           updateByIds: function(data, ids) {
-            var params = {
-              chargeId: {
-                inq: ids
-              }
-            };
-            return $q.when(
+            var deferred = $q.defer();
+            if (ids.length === 1) {
+              var params = {
+                'where[id]': ids[0].toString()
+              };
               $http.post(
                 apiRoutes.accountTransactions + '/update',
                 data,
                 {
                   params: params
                 }
-              )
-            );
+              ).then(function(response) {
+                if (response.status === 200) {
+                  deferred.resolve(response.data);
+                } else {
+                  deferred.reject({});
+                }
+              }, function(error) {
+                deferred.reject({});
+              });
+            } else if (ids.length === 0) {
+              deferred.reject({});
+            } else {
+              var params = '?where[id][inq]='
+              _.each(ids, function(id, index) {
+                params = params + id.toString() + ids.length === index+1 ? ''
+                  : '&where[id][inq]=';
+              });
+              console.log(params);
+              $http.post(
+                apiRoutes.accountTransactions + '/update' + params,
+                data
+              ).then(function(response) {
+                if (response.status === 200) {
+                  deferred.resolve(response.data);
+                } else {
+                  deferred.reject({});
+                }
+              }, function(error) {
+                deferred.reject({});
+              });
+            }
+            return deferred.promise;
           },
           getAllThen: function(enterpriseId, year, month) {
             var params = {
